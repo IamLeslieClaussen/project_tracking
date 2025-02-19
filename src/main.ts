b98d998d5d30a1547aac532a8568b2d10a1cc4ec
Leslie Claussen <leslie.claussen@mail.com>
2025-02-19 18:45:44 +0100

    latest commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById(
+    "menu-toggle"
+  ) as HTMLButtonElement;
+  const dropdownMenu = document.getElementById(
+    "dropdown-menu"
+  ) as HTMLDivElement;
+  const menuIcon = document.getElementById("menu-icon");
+
+  let isMenuOpen = false;
+
+  menuToggle.addEventListener("click", () => {
+    isMenuOpen = !isMenuOpen;
+    dropdownMenu.classList.toggle("hidden");
+
+    menuIcon.src = isMenuOpen
+      ? "./images/icon-close.svg"
+      : "./images/icon-hamburger.svg";
+  });
+});
